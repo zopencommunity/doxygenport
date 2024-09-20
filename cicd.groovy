06bc8b1b0a7c59315c6df42fff135437d05ae1ba
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:38 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/doxygenport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/doxygenport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/doxygenport.git'), string(name: 'PORT_DESCRIPTION', value: 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources, but it also supports other popular programming languages such as C, Objective-C, C#, PHP, Java, Python, IDL (Corba, Microsoft, and UNO/OpenOffice flavors), Fortran, and to some extent D. Doxygen also supports the hardware description language VHDL.' ), string(name: 'NODE_LABEL', value: "v3r1" ) ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/doxygenport.git'), string(name: 'PORT_DESCRIPTION', value: 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources, but it also supports other popular programming languages such as C, Objective-C, C#, PHP, Java, Python, IDL (Corba, Microsoft, and UNO/OpenOffice flavors), Fortran, and to some extent D. Doxygen also supports the hardware description language VHDL.' ), string(name: 'NODE_LABEL', value: "v3r1" ) ]
   }
 }
